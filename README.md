5f811f75826f820efc073dc8f9984c8a960e5fe0
zy <52zypd@gmail.com>
2021-10-09 21:35:40 +0800

    :sparkles: feat(home): 标签气泡

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ pnpm add -D vite-plugin-compression
 ```
 
 - [ ] 设置“关于”的url
+- [ ] 标签气泡
--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,13 +1,39 @@
+import { useState, useEffect } from 'react';
 import { Icon } from '@iconify/react';
 import { Link } from 'react-router-dom';
+import { Popover, Tag } from 'antd';
+import axios from 'axios';
+import type ITag from '@/utils/types/ITag';
 
 export default function Nav() {
+  const [tagPop, setTagPop] = useState<JSX.Element>();
+  useEffect(() => {
+    axios
+      .get('/tag')
+      .then((res: any) => {
+        const tagList = res.data.data.list;
+        const tagPopList = tagList.map((tag: ITag) => (
+          <Tag>
+            <Link to={`/tag/${tag.id}`} className='text3'>
+              {tag.label}
+            </Link>
+          </Tag>
+        ));
+        setTagPop(<div>{tagPopList}</div>);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className='nav'>
-      <div className='nav_item'>
-        <Icon icon='icon-park:tag' className='wh3' />
-        <div className='text3'>标签</div>
-      </div>
+      <Popover content={tagPop}>
+        <div className='nav_item'>
+          <Icon icon='icon-park:tag' className='wh3' />
+          <div className='text3'>标签</div>
+        </div>
+      </Popover>
       <Link to='/note/1'>
         <div className='nav_item'>
           <Icon icon='icon-park:avatar' className='wh3' />
