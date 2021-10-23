326a903ce4208c51265c3541d7541f260e699c39
zy <52zypd@gmail.com>
2021-10-23 17:09:23 +0800

    :sparkles: feat: 404页

--- src/pages/Error/NotFound.tsx
+++ src/pages/Error/NotFound.tsx
@@ -1,3 +1,18 @@
+import { Result, Button } from 'antd';
+
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <div className='bg_mi flex_center_p0 min-h-screen'>
+      <Result
+        status='404'
+        title='没有该页面'
+        subTitle='对不起，你想访问的页面不存在。'
+        extra={
+          <Button type='primary' shape='round' href='/'>
+            回首页
+          </Button>
+        }
+      />
+    </div>
+  );
 }
